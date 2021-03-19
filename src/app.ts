981ac25689fb4abc5a472b7aee7c6da220ce91a5
Vitalii Shvetsov <vitalya199529@gmail.com>
2021-03-19 21:12:11 +0900

    Fix issue with configuration.

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,10 @@ const app: Express = express()
 const PORT: string | number = process.env.PORT || 4000
 
 app.use(cors())
+app.use(express.json())
 app.use(router)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ufxlm.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00.ufxlm.mongodb.net:27017,cluster0-shard-00-01.ufxlm.mongodb.net:27017,cluster0-shard-00-02.ufxlm.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=atlas-12rdsz-shard-0&authSource=admin&retryWrites=true&w=majority`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 
 mongoose.set("useFindAndModify", false)
